bf6cc6de40fa1272595f4189a45082dfd0dc6e50
dongha <kimdongha15@naver.com>
2022-12-18 00:37:52 +0900

    style: 140107 JAVA 풀이 추가

--- Lv.2/140107/JAVA/imsosleepy.java
+++ Lv.2/140107/JAVA/imsosleepy.java
@@ -4,12 +4,12 @@ class Solution {
         int x = 0;
         int y = d;
         if(d % k != 0){
-            y = d - d%k;
+            y = d - d % k;
         }
 
         while(x<=d) {
             if(Math.pow(x,2) + Math.pow(y,2) <= Math.pow(d,2)) {
-                answer = answer + y/k + 1;
+                answer = answer + y / k + 1;
                 x = x + k;
             } else {
                 y = y - k;
