9460a7242988fcbde7f5be5cb9b9d33e66b6809c
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-02-06 13:40:52 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,22 @@
+const input = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+
+let sugar = input;
+let fiveBag = parseInt(sugar / 5);
+let threeBag;
+let answer;
+
+while (0 <= fiveBag) {
+    const remain = sugar - (fiveBag * 5);
+    const dividable = remain % 3 === 0;
+    
+    if (dividable) {
+        threeBag = remain / 3;
+        answer = threeBag + fiveBag;
+        break;
+    }
+
+    answer = -1;
+    fiveBag -= 1;
+}
+
+console.log(answer);
