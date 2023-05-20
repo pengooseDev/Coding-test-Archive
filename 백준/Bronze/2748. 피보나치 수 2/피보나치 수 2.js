2e600a9764565f61afe0a728d00d5fc629f5a756
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-05-21 01:53:00 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.js
@@ -0,0 +1,12 @@
+const input = parseInt(require("fs").readFileSync("/dev/stdin").toString().trim());
+const cache = new Map();
+
+const fibo = (input) => {
+    if (input === 0) return 0;
+    if (input === 1) return 1;
+    if (!cache.has(input)) cache.set(input, BigInt(fibo(input - 1)) + BigInt(fibo(input - 2)));
+
+    return cache.get(input);
+}
+
+console.log(fibo(input).toString());
