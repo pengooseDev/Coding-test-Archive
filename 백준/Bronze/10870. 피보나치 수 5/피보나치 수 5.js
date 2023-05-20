dacb23953a18a731bdc86d834fad71842d65de29
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-05-21 01:47:05 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -1,12 +1,12 @@
 const input = parseInt(require("fs").readFileSync("/dev/stdin").toString().trim());
-const cache = new Array(input).fill(-1);
+const cache = new Map();
 
 const fibo = (input) => {
     if (input === 0) return 0;
     if (input === 1) return 1;
-    if (!cache[input] !== -1) cache[input] = fibo(input - 1) + fibo(input - 2);
+    if (!cache.has(input)) cache.set(input, fibo(input - 1) + fibo(input - 2));
 
-    return cache[input];
+    return cache.get(input);
 }
 
 console.log(fibo(input));
