04dabf2f23372f2979fd5f2216d7ce15a09b2c05
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-05-30 19:36:17 +0900

    [Silver III] Title: 수열, Time: 200 ms, Memory: 17516 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.js
+++ 백준/Silver/2559. 수열/수열.js
@@ -1,16 +1,28 @@
 const [order, temp] = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
 const [_, size] = order.split(' ').map(Number);
 const arr = temp.split(' ').map(Number);
-const cache = [];
-cache[0] = 0;
 
-for (let i = 1; i <= arr.length; i++) cache[i] = cache[i - 1] + arr[i - 1];
+let window = [...arr].splice(0, size).reduce((acc, curr) => acc + curr);
+let max = window;
 
-let max = Number.MIN_SAFE_INTEGER;
-
-for (let i = 0; i <= arr.length - size; i++) {
-    max = Math.max(max, cache[i + size] - cache[i]);
-
+for (let i = size; i <= arr.length - 1; i++) {
+    window = window + arr[i] - arr[i - size];
+    max = Math.max(max, window);
 }
 
-console.log(max);
+console.log(max)
+
+
+// const cache = [];
+// cache[0] = 0;
+
+// for (let i = 1; i <= arr.length; i++) cache[i] = cache[i - 1] + arr[i - 1];
+
+// let max = Number.MIN_SAFE_INTEGER;
+
+// for (let i = 0; i <= arr.length - size; i++) {
+//     max = Math.max(max, cache[i + size] - cache[i]);
+
+// }
+
+// console.log(max);
