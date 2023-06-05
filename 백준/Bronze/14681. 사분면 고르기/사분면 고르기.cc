e191059d0de56fda575db7cba605dc9c4d75ea5e
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-06-05 14:50:17 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -4,8 +4,7 @@ int main()
 {
   int x, y, answer;
   scanf("%d %d", &x, &y);
-  answer = x > 0 ? y > 0 ? 1 : 4 : y > 0 ? 2 : 3;
-  printf("%d", answer);
+  printf("%d", x > 0 ? y > 0 ? 1 : 4 : y > 0 ? 2 : 3);
 
   return 0;
 }
