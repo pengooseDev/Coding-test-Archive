85c5da4b5f1c90291a55cda21a46a1d28064f28e
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-06-08 01:16:46 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 0 ms, Memory: 1228 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stack>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+  stack<int> s;
+  char T[50];
+  scanf("%s", T);
+
+  int T_length = strlen(T);
+  for (int i = 0; i < T_length; i++)
+  {
+    if (!s.empty() && s.top() == '(' && T[i] == ')')
+    {
+      s.pop();
+    }
+    else
+    {
+      s.emplace(T[i]);
+    }
+  }
+
+  printf("%d", s.size());
+
+  return 0;
+}
