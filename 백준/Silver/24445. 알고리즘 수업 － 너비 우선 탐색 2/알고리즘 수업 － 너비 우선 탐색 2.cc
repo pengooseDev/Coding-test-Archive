2b57c34c45f76735c48ee9885f2c633b651676c5
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-06-12 23:44:48 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 220 ms, Memory: 9100 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+bool cmp(int a, int b)
+{
+  return a > b;
+}
+
+int main()
+{
+  int N, M, R;
+  cin >> N >> M >> R;
+  vector<vector<int>> graph(N + 1);
+  vector<int> visited(N + 1, 0);
+  vector<int> answer(N + 1, 0);
+  int cnt = 1;
+
+  for (int i = 1; i <= M; i++)
+  {
+    int s, e;
+    cin >> s >> e;
+    graph[s].push_back(e);
+    graph[e].push_back(s);
+  }
+
+  for (int i = 1; i <= N; i++)
+    sort(graph[i].begin(), graph[i].end(), cmp);
+
+  queue<int> q;
+  visited[R] = 1;
+  q.push(R);
+
+  while (!q.empty())
+  {
+    int curr = q.front();
+    q.pop();
+    answer[curr] = cnt++;
+
+    for (int i : graph[curr])
+    {
+      if (!visited[i])
+      {
+        visited[i] = 1;
+        q.push(i);
+      }
+    }
+  }
+
+  for (int i = 1; i <= N; i++)
+    cout << answer[i] << "\n";
+
+  return 0;
+}
