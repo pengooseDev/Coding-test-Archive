9a8fbc6631bf7a452668222ec8b3ca83919fa24f
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-06-16 07:00:23 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 228 ms, Memory: 15876 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void dfs(vector<int> &v, vector<vector<int>> &g, int s, vector<int> &d)
+{
+  v[s] = 1;
+  for (int i : g[s])
+  {
+    if (!v[i])
+    {
+      d[i] = d[s] + 1;
+      dfs(v, g, i, d);
+    }
+  }
+}
+
+int main()
+{
+  int N, M, R;
+  cin >> N >> M >> R;
+  vector<vector<int>> graph(N + 1);
+  vector<int> visited(N + 1, 0);
+
+  for (int i = 0; i < M; i++)
+  {
+    int s, e;
+    cin >> s >> e;
+    graph[s].push_back(e);
+    graph[e].push_back(s);
+  }
+
+  vector<int> depth(N + 1, -1);
+  depth[R] = 0;
+
+  for (int i = 0; i < N + 1; i++)
+    sort(graph[i].begin(), graph[i].end());
+
+  dfs(visited, graph, R, depth);
+
+  for (int i = 1; i <= N; i++)
+    cout << depth[i] << "\n";
+
+  return 0;
+}
