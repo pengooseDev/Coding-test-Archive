a2ead59d36043c69d6c08871c6c97cd3871b7c57
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-07-01 17:32:21 +0900

    [Platinum III] Title: 님 게임 나누기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/11872. 님 게임 나누기/님 게임 나누기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int grundy(int P)
+{
+  if (P % 4 == 0)
+    return P - 1;
+
+  if (P % 4 == 3)
+    return P + 1;
+
+  return P;
+}
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  int sum = 0;
+
+  for (int i = 0; i < N; i++)
+  {
+    int P;
+    cin >> P;
+
+    sum ^= grundy(P);
+  }
+
+  sum != 0 ? cout << "koosaga" << endl : cout << "cubelover" << endl;
+
+  return 0;
+}
