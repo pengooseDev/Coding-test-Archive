7db5c2d90e2cbb3fb49722a937d4d123aacf5130
Pengoose <pengoose_dev@naver.com>
2023-08-03 22:23:27 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1336 ms, Memory: 245556 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.js
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.js
@@ -1,5 +1,2 @@
 const [_, ...input] = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n').map(Number);
-
-const mySet = new Set();
-for (let i of input) mySet.add(i);
-console.log([...mySet].sort((a, b) => b - a).join('\n'));
+console.log(input.sort((a, b) => b - a).join('\n'));
