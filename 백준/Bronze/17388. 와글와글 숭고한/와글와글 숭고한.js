737cb1ac0ed484877a6b9ef0bf01c05f60c48b1a
Pengoose <pengoose_dev@naver.com>
2023-09-14 22:38:46 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.js
@@ -0,0 +1,2 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(' ').map(Number);
+console.log(input.reduce((a,b)=>a+b) >= 100 ? 'OK' : ['Soongsil','Korea','Hanyang'][[input.indexOf(Math.min(...input))]]);
